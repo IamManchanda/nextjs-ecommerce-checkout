3d63a222c41f3a2637a6b381a66c8f3c929ccf80
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-22 23:10:58 +0530

    Stripe server client env variable renaming

--- src/utils/stripe-server-client.js
+++ src/utils/stripe-server-client.js
@@ -1,5 +1,7 @@
 import Stripe from "stripe";
 
-const stripeServerClient = new Stripe(process.env.NEXT_PUBLIC_STRIPE_API_KEY);
+const stripeServerClient = new Stripe(
+  process.env.NEXT_PUBLIC_STRIPE_API_SECRET_KEY,
+);
 
 export default stripeServerClient;
