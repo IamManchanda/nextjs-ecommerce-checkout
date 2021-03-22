a2f7af109c1b0abfb473a98b7b1eeb44e1b61169
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-22 16:18:31 +0530

    Starting Work on Next.js Starter

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function PageIndex() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js eCommerce Checkout</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Next.js eCommerce Checkout</h1>
+    </div>
+  );
+}
+
+export default PageIndex;
